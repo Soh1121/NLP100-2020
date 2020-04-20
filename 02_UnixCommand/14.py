985cef96fc7135206a5fdad422d3431e7c2c0077
Soh1121 <satou.shg@gmail.com>
2020-04-20 14:25:35 +0900

    [fix]コマンドライン引数を取るように変更

--- 02_UnixCommand/14.py
+++ 02_UnixCommand/14.py
@@ -1,4 +1,11 @@
-n = int(input())
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Set a argument N, for example '$ python 14.py 3'.")
+    sys.exit()
+
+n = int(sys.argv[1])
 file_name = "popular-names.txt"
 
 with open(file_name) as rf:
