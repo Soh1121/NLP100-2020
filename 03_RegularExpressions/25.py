be03eae52514c0faf5143293f3fb0569e84ba60f
Soh1121 <satou.shg@gmail.com>
2020-05-02 12:08:11 +0900

    [update]リファクタリング

--- 03_RegularExpressions/25.py
+++ 03_RegularExpressions/25.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import re
 
+
+def basic_info_extraction(text):
+    texts = text.split("\n")
+    index = texts.index("{{基礎情報 国")
+    basic_info = []
+    for i in texts[index + 1:]:
+        if i == "}}":
+            break
+        if i.find("|") != 0:
+            basic_info[-1] += ", " + i
+            continue
+        basic_info.append(i)
+
+    pattern = r"\|(.*)\s=(.*)"
+    ans = {}
+    for i in basic_info:
+        result = re.match(pattern, i)
+        ans[result.group(1)] = result.group(2).lstrip(" ")
+    return ans
+
+
 data_frame = pd.read_json("jawiki-country.json.gz", compression="infer", lines=True)
 uk_text = data_frame.query("title == 'イギリス'")['text'].values[0]
 
-uk_texts = uk_text.split("\n")
-index = uk_texts.index("{{基礎情報 国")
-basic_info = []
-for i in uk_texts[index + 1:]:
-    if i == "}}":
-        break
-    if i.find("|") != 0:
-        basic_info[-1] += ", " + i
-        continue
-    basic_info.append(i)
-
-pattern = r"\|(.*)\s=(.*)"
-ans = {}
-for i in basic_info:
-    result = re.match(pattern, i)
-    ans[result.group(1)] = result.group(2).lstrip(" ")
-
+ans = basic_info_extraction(uk_text)
 for key, value in ans.items():
     print(key, ":", value)
--- 03_RegularExpressions/26.py
+++ 03_RegularExpressions/26.py
@@ -1,29 +1,38 @@
 import pandas as pd
 import re
 
+
+def basic_info_extraction(text):
+    texts = text.split("\n")
+    index = texts.index("{{基礎情報 国")
+    basic_info = []
+    for i in texts[index + 1:]:
+        if i == "}}":
+            break
+        if i.find("|") != 0:
+            basic_info[-1] += ", " + i
+            continue
+        basic_info.append(i)
+
+    pattern = r"\|(.*)\s=(.*)"
+    ans = {}
+    for i in basic_info:
+        result = re.match(pattern, i)
+        ans[result.group(1)] = result.group(2).lstrip(" ")
+    return ans
+
+
+def remove_emphasis(value):
+    pattern = r"(.*?)'{1,3}(.+?)'{1,3}(.*)"
+    result = re.match(pattern, value)
+    if result is not None:
+        return "".join(result.group(1, 2, 3))
+
+
 data_frame = pd.read_json("jawiki-country.json.gz", compression="infer", lines=True)
 uk_text = data_frame.query("title == 'イギリス'")['text'].values[0]
 
-uk_texts = uk_text.split("\n")
-index = uk_texts.index("{{基礎情報 国")
-basic_info = []
-for i in uk_texts[index + 1:]:
-    if i == "}}":
-        break
-    if i.find("|") != 0:
-        basic_info[-1] += ", " + i
-        continue
-    basic_info.append(i)
-
-value_pattern = r"\|(.*)\s=(.*)"
-emphasis_pattern = r"(.*?)'{1,3}(.+?)'{1,3}(.*)"
-ans = {}
-for i in basic_info:
-    result = re.match(value_pattern, i)
-    emphasis_result = re.match(emphasis_pattern, result.group(2).lstrip(" "))
-    if emphasis_result is not None:
-        ans[result.group(1)] = emphasis_result.group(1) + emphasis_result.group(2) + emphasis_result.group(3)
-        continue
-    ans[result.group(1)] = result.group(2).lstrip(" ")
+ans = basic_info_extraction(uk_text)
 for key, value in ans.items():
+    remove_emphasis(value)
     print(key, ":", value)
