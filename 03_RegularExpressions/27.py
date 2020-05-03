bcd0c24a3a285a60896f135101d88d8aa2bd4e4a
Soh1121 <satou.shg@gmail.com>
2020-05-03 12:12:21 +0900

    [update]リファクタリングを実施

--- 03_RegularExpressions/27.py
+++ 03_RegularExpressions/27.py
@@ -1,37 +1,56 @@
 import pandas as pd
 import re
 
+
+def basic_info_extraction(text):
+    texts = text.split("\n")
+    index = texts.index("{{基礎情報 国")
+    basic_info = []
+    for i in texts[index + 1:]:
+        if i == "}}":
+            break
+        if i.find("|") != 0:
+            basic_info[-1] += ", " + i
+            continue
+        basic_info.append(i)
+
+    pattern = r"\|(.*)\s=(.*)"
+    ans = {}
+    for i in basic_info:
+        result = re.match(pattern, i)
+        ans[result.group(1)] = result.group(2).lstrip(" ")
+    return ans
+
+
+def remove_emphasis(value):
+    pattern = r"(.*?)'{1,3}(.+?)'{1,3}(.*)"
+    result = re.match(pattern, value)
+    if result is not None:
+        return "".join(result.group(1, 2, 3))
+    else:
+        return value
+
+
+def remove_innerlink(value):
+    pipe_pattern = r"(\[\[(.*?\|)(.+?)\]\])"
+    result = re.findall(pipe_pattern, value)
+    if len(result) != 0:
+        for i in result:
+            value = value.replace(i[0], i[-1])
+    pipe_pattern = r"(\[\[(.+?)\]\])"
+    result = re.findall(pipe_pattern, value)
+    if len(result) != 0:
+        for i in result:
+            if "[[ファイル:" not in value:
+                value = value.replace(i[0], i[-1])
+    return value
+
+
 data_frame = pd.read_json("jawiki-country.json.gz", compression="infer", lines=True)
 uk_text = data_frame.query("title == 'イギリス'")['text'].values[0]
 
-uk_texts = uk_text.split("\n")
-index = uk_texts.index("{{基礎情報 国")
-basic_info = []
-for i in uk_texts[index + 1:]:
-    if i == "}}":
-        break
-    if i.find("|") != 0:
-        basic_info[-1] += ", " + i
-        continue
-    basic_info.append(i)
-
-value_pattern = r"\|(.*)\s=(.*)"
-emphasis_pattern = r"(.*?)'{1,3}(.+?)'{1,3}(.*)"
-link_pattern = r"(\[\[(.*\|)*?(.+?)\]\])"
-ans = {}
-for i in basic_info:
-    category_result = re.match(value_pattern, i)
-    emphasis_result = re.match(emphasis_pattern, category_result.group(2).lstrip(" "))
-    if emphasis_result is not None:
-        value = emphasis_result.group(1) + emphasis_result.group(2) + emphasis_result.group(3)
-        continue
-    else:
-        value = category_result.group(2).lstrip(" ")
-    link_result = re.findall(link_pattern, value)
-    if len(link_result) != 0:
-        if "[[ファイル:" not in value:
-            for i in link_result:
-                value = value.replace(i[0], i[-1])
-    ans[category_result.group(1)] = value
+ans = basic_info_extraction(uk_text)
 for key, value in ans.items():
+    value = remove_emphasis(value)
+    value = remove_innerlink(value)
     print(key, ":", value)
