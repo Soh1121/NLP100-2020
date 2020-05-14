2db9d75227bdd022c66cfbfa42cbf3332179083d
Soh1121 <satou.shg@gmail.com>
2020-05-14 10:29:26 +0900

    [add]2020/05/14実施分

---
+++ 04_MorphologicalAnalysis/32.py
@@ -0,0 +1,34 @@
+def parse(sentence):
+    morphemes = []
+    words = sentence.split("\n")
+    words = [i for i in words if i != ""]
+    for word in words:
+        result = {}
+        result["surface"], info = extraction_surface(word)
+        result["base"] = info[6]
+        result["pos"] = info[0]
+        result["pos1"] = info[1]
+        morphemes.append(result)
+    return morphemes
+
+
+def extraction_surface(word):
+    morpheme = word.split("\t")
+    return morpheme[0], morpheme[1].split(",")
+
+
+file_name = "./output/neko.txt.mecab"
+with open(file_name) as rf:
+    sentences = rf.read().split("EOS\n")
+
+result = []
+for sentence in sentences:
+    if len(parse(sentence)) != 0:
+        result.append(parse(sentence))
+
+answer = []
+for sentence in result:
+    verbs = list(filter(lambda x: x["pos"] == "動詞", sentence))
+    answer += [verb["base"] for verb in verbs]
+
+print("\n".join(answer))
