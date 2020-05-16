0098f5f1d9b0951ae0e0a156aaf9c3e52e6cdcce
Soh1121 <satou.shg@gmail.com>
2020-05-16 16:04:44 +0900

    [fix]不要な部分を整理

--- 03_RegularExpressions/21.py
+++ 03_RegularExpressions/21.py
@@ -5,7 +5,7 @@ file_name = "jawiki-country.json.gz"
 data_frame = pd.read_json(file_name, compression='infer', lines=True)
 uk_text = data_frame.query('title == "イギリス"')['text'].values[0]
 
-pattern = r'\[\[Category:(.*)(|.*)*\]\]'
+pattern = r'\[\[Category:(.*)\]\]'
 for line in uk_text.split("\n"):
     result = re.match(pattern, line)
     if result is not None:
