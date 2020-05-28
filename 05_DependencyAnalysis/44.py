8f34a5b10d32585d6d3c82316c440241cbf93d61
Soh1121 <satou.shg@gmail.com>
2020-05-28 11:06:26 +0900

    [add]2020/05/28実施分

---
+++ 05_DependencyAnalysis/44.py
@@ -0,0 +1,87 @@
+import copy
+import pydot
+
+
+class Morph():
+    def __init__(self, surface, prop):
+        self.surface = surface
+        self.base = prop[6]
+        self.pos = prop[0]
+        self.pos1 = prop[1]
+
+    def __repr__(self):
+        ans = ""
+        ans += "surface: " + self.surface
+        ans += "\tbase: " + self.base
+        ans += "\tpos: " + self.pos
+        ans += "\tpos1: " + self.pos1
+        return ans
+
+
+class Chunk():
+    def __init__(self, chunk):
+        self.morphs = copy.copy(chunk["morphs"])
+        self.dst = chunk["dst"]
+        self.srcs = copy.copy(chunk["srcs"])
+
+
+def parse(sentence):
+    words = [i for i in sentence.split("\n") if i != ""]
+    chunk = {}
+    info = []
+    for word in words:
+        if word[0] == "*":
+            info = word.split()
+            if info[1] not in chunk:
+                chunk[info[1]] = {
+                    "morphs": [],
+                    "srcs": []
+                }
+            chunk[info[1]]["dst"] = info[2]
+            if info[2] == "-1D":
+                continue
+            if info[2][0] not in chunk:
+                chunk[info[2][0]] = {
+                    "morphs": [],
+                    "srcs": [info[1]]
+                }
+            else:
+                chunk[info[2][0]]["srcs"].append(info[1])
+        else:
+            arg = word.split("\t")
+            chunk[info[1]]["morphs"].append(Morph(arg[0], arg[1].split(",")))
+    return chunk
+
+
+def create_text(morphs):
+    value = ""
+    for morph in morphs:
+        if morph.pos != "記号":
+            value += morph.surface
+    return value
+
+
+file_name = "./output/neko.txt.cabocha"
+with open(file_name) as rf:
+    sentences = rf.read().split("EOS\n")
+sentences = list(filter(lambda x: x != "", sentences))
+
+result = []
+for sentence in sentences:
+    block = []
+    chunk = parse(sentence)
+    chunk_sorted = sorted(chunk.items(), key=lambda x: x[0])
+    for i in chunk_sorted:
+        block.append(Chunk(i[1]))
+    result.append(block)
+
+pairs = []
+target = result[3]
+for clause in target:
+    present_text = create_text(clause.morphs)
+    if clause.dst != "-1D":
+        to_number = int(clause.dst[0])
+        to_text = create_text(target[to_number].morphs)
+        pairs.append([present_text, to_text])
+graph = pydot.graph_from_edges(pairs, directed=True)
+graph.write_png("./output/44.png", prog="dot")
